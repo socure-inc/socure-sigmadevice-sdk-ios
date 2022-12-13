fd8fe9ccc063966a7a5c9e15cbe46a9f727c66a9
Pavan Mellamputi <pavan.mellamputi@socure.com>
2022-12-13 12:52:34 -0800

    MDB-288 Add support for Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+let package = Package(
+    name: "SigmaDevice",
+    platforms: [
+           .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SigmaDevice",
+            targets: ["SigmaDeviceWrapper"]),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "DeviceRisk",
+            path: "Framework/DeviceRisk.xcframework"
+        ),
+        .binaryTarget(
+            name: "TrustKit",
+            path: "Framework/TrustKit.xcframework"
+        ),
+        .target(
+            name: "SigmaDeviceWrapper",
+            dependencies: [
+                .target(name: "DeviceRisk"),
+                .target(name: "TrustKit")
+            ],
+            path: "SigmaDeviceWrapper"
+        ),
+    ]
+)
