e26e51702cb2ba8b6a43bf1139c757f9859ae311
Ryan C. Payne <ryan.payne@socure.com>
2024-09-19 12:51:30 -0400

    Updated for the 4.3.2 release

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "DeviceRisk",
-            url: "https://sdk.socure.com/socure-sdks/sigma-device/ios/socure-device-risk-4.3.1.zip",
-            checksum: "11f52e0a76d563385471f3bf3b86921b360de4285b31a4a2a199ddcc4adbfc56"
+            url: "https://sdk.socure.com/socure-sdks/sigma-device/ios/socure-device-risk-4.3.2.zip",
+            checksum: "2322cd35c80a7adec68b17a3ccb74b70cde34c40c8c96449a6c689f202a6e354"
         )
     ]
 )
